605123bf5582de43933be434f1d868886b418aed
FellipeWeb <80231894+fellipe2365@users.noreply.github.com>
2022-05-26 21:02:52 -0300

    Update animaçaoText.js

--- animaçaoText.js
+++ animaçaoText.js
@@ -1,6 +1,6 @@
 var el = document.querySelector("#animacao");
 var text = "Olá, me chamo Fellipe Ramos e acredito que esta empresa é um lugar onde os profissionais são valorizados e têm oportunidades de mostrar seu potencial e talentos. Eu quero ter esta chance e colaborar com minhas experiências, habilidades, conhecimentos e dedicação para que a organização continue crescendo e obtendo sempre grandes resultados. Minha intenção é crescer junto e também ter sucesso em minha carreira fazendo parte desta equipe."
-var interval = 100;
+var interval = 190;
 function showtext(el, text, interval) {
   var char = text.split("").reverse();
   
